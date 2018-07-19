93965bcfdc2073b19a72e59418deff222e19b125
Daniel Coria <daniel.coria@accedo.tv>
2018-07-18 23:56:30 -0500

    added uitableviewcontroller class and delegates fixed ios 11 added navigation in storyboard

--- SwiftCoreData/SwiftCoreData/ViewController.swift
+++ SwiftCoreData/SwiftCoreData/ViewController.swift
@@ -8,14 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    
+    var items : [String] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        // hello world
+        navigationController?.navigationBar.barTintColor = UIColor(red: 27/255, green: 165/255, blue: 225/255, alpha: 1)
+        navigationController?.navigationBar.tintColor = .white
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addNewUser))
+        navigationItem.title = "Core Data"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
     }
-
-
+    
+    @objc func addNewUser(){
+        
+        let alertController = UIAlertController(title: "new brand", message: "Fill with one brand you love", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default)
+        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
+        alertController.addTextField(configurationHandler: nil)
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        present(alertController, animated: true, completion: nil)
+    }
+    
+    // alert action
+    func showAlert(title: String, message: String){
+        let alert = UIAlertController(title:title, message: message, preferredStyle: .alert)
+        let cancel = UIAlertAction(title: "Ok", style: .cancel, handler: nil)
+        alert.addAction(cancel)
+        present(alert, animated: true, completion: nil)
+    }
+    
+    // tableview delegates
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        return cell
+        
+    }
+    
 }
 
