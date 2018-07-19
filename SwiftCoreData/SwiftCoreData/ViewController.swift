b2f66d8e78bb07268e64006ae092ae9c37c9e7f6
Daniel Coria <daniel.coria@accedo.tv>
2018-07-19 00:21:55 -0500

    import coredata added entities coredata model save user get user

--- SwiftCoreData/SwiftCoreData/ViewController.swift
+++ SwiftCoreData/SwiftCoreData/ViewController.swift
@@ -7,30 +7,74 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UITableViewController {
     
     var items : [String] = []
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
         navigationController?.navigationBar.barTintColor = UIColor(red: 27/255, green: 165/255, blue: 225/255, alpha: 1)
         navigationController?.navigationBar.tintColor = .white
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add", style: .plain, target: self, action: #selector(addNewUser))
         navigationItem.title = "Core Data"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        getUser()
+        
     }
     
     @objc func addNewUser(){
         
         let alertController = UIAlertController(title: "new brand", message: "Fill with one brand you love", preferredStyle: .alert)
-        let saveAction = UIAlertAction(title: "Save", style: .default)
+        let saveAction = UIAlertAction(title: "Save", style: .default){ action in
+            
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let managedObjectContext = appDelegate.persistentContainer.viewContext
+            
+            let entity = NSEntityDescription.entity(forEntityName: "User", in: managedObjectContext)
+            let newUser = NSManagedObject(entity: entity!, insertInto: managedObjectContext)
+            
+            newUser.setValue("apple", forKey: "username")
+            newUser.setValue("12345", forKey: "password")
+            newUser.setValue("20", forKey: "age")
+            
+            appDelegate.persistentContainer.performBackgroundTask { (context) in
+                appDelegate.saveContext()
+            }
+            
+        }
         let cancelAction = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
         alertController.addTextField(configurationHandler: nil)
         alertController.addAction(saveAction)
         alertController.addAction(cancelAction)
         present(alertController, animated: true, completion: nil)
+    }
+    
+    func getUser(){
+        DispatchQueue.main.async {[weak self] in
+            
+            let request = NSFetchRequest<NSFetchRequestResult>(entityName: "User")
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else { return }
+            let managedObjectContext = appDelegate.persistentContainer.viewContext
+            
+            do {
+                
+                let userCreated = try managedObjectContext.fetch(request) as! [NSManagedObject]
+                
+                for data in userCreated{
+                    let value = data.value(forKey: "username") as! String
+                    print(value)
+                    print("_____")
+                }
+                
+            } catch  {
+                print("failed fetch")
+            }
+        }
     }
     
     // alert action
